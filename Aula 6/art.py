2aa336c000e43c0a1b5f330d16c0b7d4a657303e
Fabioomega <fabioms2002@gmail.com>
2026-04-08 10:10:51 -0300

    Added activities for class 6

---
+++ Aula 6/art.py
@@ -0,0 +1,5 @@
+# Você está tentando fazer uma peça artistica beseado nas imagens do James Webb Telescope.
+# Você deve primeiro extrair o fundo preto da imagem e substitui-lo por uma das três cores em RGB aleatoriamente:
+# [(194,211,205), (159,164,169), (86,73,76)]
+
+# Nota: Lembre-se que por padrão o opencv abre a imagem em BGR em vez de RGB!
---
+++ Aula 6/xray-norm.py
@@ -0,0 +1,3 @@
+# Você está tentando processar multiplos raio-x para detectar anomalias. Infelizmente alguns raio-x não estão no formato certo para deixar sua pesquisa fácil.
+# Geralmente raio-x usam as cores mais escuras para representar as áreas menos densas da imagem, enquanto as cores claras representam áreas de alta densidade, como ossos.
+# Transforme a imagem xray-inverted.jpg para que siga o mesmo padrão, ossos brancos e fundo preto, em vez da estrutura atual.
